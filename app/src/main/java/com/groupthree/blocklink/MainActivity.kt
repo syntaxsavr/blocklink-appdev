788cd7893a0ab99a67e97a0f94f6a93db023e023
Cathrin <cathrin_s@hotmail.com>
2023-07-05 21:03:23 +0200

    Added MarketFragment to Main Activity

--- app/src/main/java/com/groupthree/blocklink/MainActivity.kt
+++ app/src/main/java/com/groupthree/blocklink/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.groupthree.blocklink
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.groupthree.blocklink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
     }
 }
---
+++ app/src/main/java/com/groupthree/blocklink/MarketFragment.kt
@@ -0,0 +1,21 @@
+package com.groupthree.blocklink
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.groupthree.blocklink.databinding.MarketFragmentBinding
+
+class MarketFragment : Fragment() {
+
+    private lateinit var binding: MarketFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.market_fragment, container, false)
+    }
+}
