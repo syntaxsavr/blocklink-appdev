5e358dff674cd7274b1f916c79225999e4040843
Samuel <87152786+syntaxsavr@users.noreply.github.com>
2023-07-21 13:40:38 +0200

    Functionalize MainAcitivity
    Added MainActivity and designed and functionalized it
    Added BlankFragment and designed it
    Added EventsFragment

--- app/src/main/java/com/groupthree/blocklink/MainActivity.kt
+++ app/src/main/java/com/groupthree/blocklink/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.groupthree.blocklink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
+/**
+ * This class is the main activity of the app. It is the first screen that the user sees when they open the app.
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var alertDialog: AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Loads the fragment_chat.xml layout into the fragmentContainer
+     */
+    fun loadFramentChat(view: View) {
+        // initialize fragment manager and transaction
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val eventsFragment = EventsFragment()
+
+        // replace the fragment_container with the fragment on click
+        fragmentTransaction.replace(R.id.fragmentContainer, eventsFragment)
+        fragmentTransaction.commit()
+    }
+
+    fun logout(view: View) {
+// Create an AlertDialog.Builder object
+        val builder = AlertDialog.Builder(this)
+
+        // Set the title
+        builder.setTitle("Logout")
+
+        // Set the message
+        builder.setMessage("Are you sure you want to logout?")
+
+        // Set the positive button
+        builder.setPositiveButton("Yes") { _, _ ->
+            // Logout the user
+            //TODO: Implement logout functionality
+            alertDialog.dismiss()
+        }
+
+        // Set the negative button
+        builder.setNegativeButton("No") { dialog, which ->
+            // Do nothing
+            dialog.dismiss()
+        }
+
+        // Show the dialog
+        alertDialog = builder.create()
+        alertDialog.show()
     }
 }
